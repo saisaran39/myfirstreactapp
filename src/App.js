ef905dd01ced24b6bd040a0965abd2ec4596f670
saisaran39 <sai.saran72@gmail.com>
2024-12-04 19:55:19 +0530

    counter example with redux

--- src/App.js
+++ src/App.js
@@ -1,67 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import HomePage from './HomePage'
-import AboutUsPage from './AboutUsPage';
-import ContactUsPage from './ContactUsPage';
-// import Refs from './Refs';
-// import Test from './TestComp';
-// import TestComponent from './TestComponent';
-// import SnapShotComp from './SnapShotComp';
-// import ParentComp from './ParentComp';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
-import { useEffect } from 'react';
-
-function App(props) {
-  useEffect(() => {
-    console.log("Page called")
-  },[props])
+import React from 'react';
+import { connect } from 'react-redux';
+import { increment, decrement } from './action';
+function App({ count, increment, decrement }) {
   return (
-    <>
-    <Router>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/about">About</Link>
-                    </li>
-                    <li>
-                        <Link to="/contact">Contact</Link>
-                    </li>
-                </ul>
-            </nav>
-            {/*Implementing Routes for respective Path */}
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/about" element={<AboutUsPage />} />
-                <Route path="/contact" element={<ContactUsPage />} />
-            </Routes>
-        </Router>
-    </>
+    <div className="App">
+      <h1>Redux Counter App</h1>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
   );
 }
-
-export default App;
-
-// import React from "react";
-
-// const name = "Learner";
-
-// const element = (
-//   <React.Fragment>
-//     <h1>
-//         Hello,
-//         {name}.Welcome to GeeksforGeeks.
-//     </h1>
-//     <h2>This is h2 tag</h2>
-//   </React.Fragment>
-// );
-
-
-// ReactDOM.render(element, document.getElementById("root"));
+const mapStateToProps = (state) => {
+  return {
+    count: state.count,
+  };
+};
+const mapDispatchToProps = {
+  increment,
+  decrement,
+};
+export default connect(mapStateToProps, mapDispatchToProps)(App);
